031a8dbbbccadbb1422ca5a1a2d284702ec513f2
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-02-24 11:41:57 -0800

    starting pet seeds and cleaning up seeds

---
+++ server/models/Pet.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const petSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0
+  }
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+
+module.exports = Pet;
--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,9 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const Pet = require('./Pet');
 
-const profileSchema = new Schema({
+const userSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -19,16 +21,12 @@ const profileSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  skills: [
-    {
-      type: String,
-      trim: true,
-    },
-  ],
+  //pets: [Pet.schema],
+  //favorites: [Pet.schema]
 });
 
 // set up pre-save middleware to create password
-profileSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -38,10 +36,10 @@ profileSchema.pre('save', async function (next) {
 });
 
 // compare the incoming password with the hashed password
-profileSchema.methods.isCorrectPassword = async function (password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const Profile = model('Profile', profileSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports = Profile;
+module.exports = User;
